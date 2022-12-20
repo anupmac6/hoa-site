4088c9c98ca88181f7b1c48fb1011408e0c0b202
anupmac6 <anupmac6@gmail.com>
2022-12-20 15:59:46 -0500

    menu changes

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -8,9 +8,40 @@ const data = [
     to: `${adminRoot}/home`,
     subs: [
       {
-        icon: 'simple-icon-paper-plane',
-        label: 'menu.start',
-        to: `${adminRoot}/home/start`,
+        id: 'home-tasks',
+        label: 'Maintenance',
+        icon: 'simple-icon-lock',
+        to: `${adminRoot}/home`,
+        subs: [
+          {
+            icon: 'simple-icon-paper-plane',
+            label: 'menu.start',
+            to: `${adminRoot}/home/start`,
+          },
+        ],
+      },
+      {
+        id: 'home-tasks',
+        label: 'Dues',
+        icon: 'simple-icon-lock',
+        to: `${adminRoot}/home`,
+        subs: [
+          {
+            icon: 'simple-icon-paper-plane',
+            label: 'Make Payment',
+            to: `${adminRoot}/home/start`,
+          },
+          {
+            icon: 'simple-icon-paper-plane',
+            label: 'Invoices',
+            to: `${adminRoot}/home/start`,
+          },
+          {
+            icon: 'simple-icon-paper-plane',
+            label: 'Manage Account',
+            to: `${adminRoot}/home/start`,
+          },
+        ],
       },
     ],
   },
@@ -21,14 +52,60 @@ const data = [
     to: `${adminRoot}/admin`,
     subs: [
       {
-        icon: 'simple-icon-people',
-        label: 'Registration Queue',
-        to: `${adminRoot}/admin/registration-queue`,
+        id: 'admin-users',
+        label: 'Users',
+        to: `${adminRoot}/admin/users`,
+        subs: [
+          {
+            icon: 'simple-icon-people',
+            label: 'Registration Queue',
+            to: `${adminRoot}/admin/users/registration-queue`,
+          },
+          {
+            icon: 'iconsminds-mens',
+            label: 'All Users',
+            to: `${adminRoot}/admin/users/all-users`,
+          },
+          {
+            icon: 'iconsminds-mens',
+            label: 'Admins',
+            to: `${adminRoot}/admin/users/all-users`,
+          },
+        ],
       },
       {
-        icon: 'iconsminds-mens',
-        label: 'All Users',
-        to: `${adminRoot}/admin/all-users`,
+        id: 'admin-tasks',
+        label: 'Maintenance',
+        to: `${adminRoot}/admin/maintenance`,
+        subs: [
+          {
+            icon: 'simple-icon-people',
+            label: 'Requests',
+            to: `${adminRoot}/admin/users/registration-queue`,
+          },
+          {
+            icon: 'iconsminds-mens',
+            label: 'Approved',
+            to: `${adminRoot}/admin/users/all-users`,
+          },
+          {
+            icon: 'iconsminds-mens',
+            label: 'Rejected',
+            to: `${adminRoot}/admin/users/all-users`,
+          },
+        ],
+      },
+      {
+        id: 'admin-dues',
+        label: 'Dues',
+        to: `${adminRoot}/admin/maintenance`,
+        subs: [
+          {
+            icon: 'simple-icon-people',
+            label: 'Dashboard',
+            to: `${adminRoot}/admin/users/registration-queue`,
+          },
+        ],
       },
     ],
   },
--- src/views/app/admin/index.js
+++ src/views/app/admin/index.js
@@ -2,11 +2,11 @@ import React, { Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 const Registrations = React.lazy(() =>
-  import(/* webpackChunkName: "start" */ './registrations')
+  import(/* webpackChunkName: "start" */ './users/registrations')
 );
 
 const AllUsers = React.lazy(() =>
-  import(/* webpackChunkName: "start" */ './allUsers')
+  import(/* webpackChunkName: "start" */ './users/allUsers')
 );
 const Admin = ({ match }) => (
   <Suspense fallback={<div className="loading" />}>
@@ -14,14 +14,14 @@ const Admin = ({ match }) => (
       <Redirect
         exact
         from={`${match.url}/`}
-        to={`${match.url}/registration-queue`}
+        to={`${match.url}/users/registration-queue`}
       />
       <Route
-        path={`${match.url}/registration-queue`}
+        path={`${match.url}/users/registration-queue`}
         render={(props) => <Registrations {...props} />}
       />
       <Route
-        path={`${match.url}/all-users`}
+        path={`${match.url}/users/all-users`}
         render={(props) => <AllUsers {...props} />}
       />
       <Redirect to="/error" />
